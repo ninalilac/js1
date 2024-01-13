8b597bf7cffea5e4dd654db05d30a55d2d8b8e5b
Nina <148767777+ninalilac@users.noreply.github.com>
2024-01-13 23:27:06 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+
+let element1 = 100;
+
+
+if (element1 < 50) {
+    console.log("naklebia 50ze");
+} else if (element1 > 20) {
+    console.log("metia 20ze")
+} else {
+    console.log("error")
+}
+
+
+let userName="mariam";
+
+if (userName == "mariam") {
+    console.log("true")
+} else {
+    console.log("false")
+}
+
+
+let userName1 = "Mariam";
+
+switch (userName1) {
+  case "Mariam":
+    console.log("true");
+    break;
+  default:
+    console.log("false");
+}
+
+
+let userName2 = "Anna"
+
+switch (userName2) {
+    case "Anna":
+    console.log("true");
+    break;
+    default:
+    console.log("false");
+}
